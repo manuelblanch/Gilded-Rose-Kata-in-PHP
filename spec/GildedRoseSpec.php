ed9639717fdf873870204f787ff7ed374554b3b5
manuelblanch <manuelblanch@iesebre.com>
2016-12-05 14:48:52 +0100

    all tets ok

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -8,15 +8,16 @@ use PhpSpec\ObjectBehavior;
  */
 class GildedRoseSpec extends ObjectBehavior
 {
+
     /**
-     * Check it is initializable_normal_item
+     *
      */
-    function it_is_initializable_normal_item()
+    function it_is_initializable()
     {
         $this->beConstructedWith('of', ['Normal', 10, 5]);
         $this->shouldHaveType(GildedRose::class);
     }
-    //Normal items Tests
+
     /**
      *
      */
@@ -28,6 +29,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(4);
     }
 
+    /**
+     *
+     */
     function it_updates_normal_items_on_the_sell_date()
     {
         $this->beConstructedThrough('of',['Normal', 10, 0]);
@@ -38,6 +42,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-1);
     }
 
+    /**
+     *
+     */
     function it_updates_normal_items_after_the_sell_date()
     {
         $this->beConstructedThrough('of',['Normal', 10, -5]);
@@ -48,6 +55,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-6);
     }
 
+    /**
+     *
+     */
     function it_updates_normal_items_with_a_quality_of_0()
     {
         $this->beConstructedThrough('of',['Normal', 0, 5]);
@@ -58,6 +68,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(4);
     }
 
+    /**
+     *
+     */
     function it_updates_Brie_items_before_the_sell_date()
     {
         $this->beConstructedThrough('of',['Aged Brie', 10, 5]);
@@ -68,6 +81,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(4);
     }
 
+    /**
+     *
+     */
     function it_updates_Brie_items_before_the_sell_date_with_maximum_quality()
     {
         $this->beConstructedThrough('of',['Aged Brie', 50, 5]);
@@ -78,6 +94,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(4);
     }
 
+    /**
+     *
+     */
     function it_updates_Brie_items_on_the_sell_date()
     {
         $this->beConstructedThrough('of',['Aged Brie', 10, 0]);
@@ -88,6 +107,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-1);
     }
 
+    /**
+     *
+     */
     function it_updates_Brie_items_on_the_sell_date_near_maximum_quality()
     {
         $this->beConstructedThrough('of',['Aged Brie', 49, 0]);
@@ -98,6 +120,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-1);
     }
 
+    /**
+     *
+     */
     function it_updates_Brie_items_after_the_sell_date()
     {
         $this->beConstructedThrough('of',['Aged Brie', 10, -10]);
@@ -108,6 +133,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-11);
     }
 
+    /**
+     *
+     */
     function it_updates_Brie_items_after_the_sell_date_with_maximum_quality()
     {
         $this->beConstructedThrough('of',['Aged Brie', 50, -10]);
@@ -118,6 +146,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-11);
     }
 
+    /**
+     *
+     */
     function it_updates_Sulfuras_items_before_the_sell_date()
     {
         $this->beConstructedThrough('of',['Sulfuras, Hand of Ragnaros', 10, 5]);
@@ -128,6 +159,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(5);
     }
 
+    /**
+     *
+     */
     function it_updates_Sulfuras_items_before_on_the_sell_date()
     {
         $this->beConstructedThrough('of',['Sulfuras, Hand of Ragnaros', 10, 5]);
@@ -138,6 +172,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(5);
     }
 
+    /**
+     *
+     */
     function it_updates_Sulfuras_items_after_the_sell_date()
     {
         $this->beConstructedThrough('of',['Sulfuras, Hand of Ragnaros', 10, -1]);
@@ -148,6 +185,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-1);
     }
 
+    /**
+     *
+     */
     function it_updates_Backstage_pass_items_long_before_the_sell_date()
     {
         $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, 11]);
@@ -158,6 +198,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(10);
     }
 
+    /**
+     *
+     */
     function it_updates_Backstage_pass_items_close_to_the_sell_date()
     {
         $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, 10]);
@@ -168,6 +211,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(9);
     }
 
+    /**
+     *
+     */
     function it_updates_Backstage_pass_items_close_to_the_sell_data_at_max_quality()
     {
         $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 50, 10]);
@@ -178,6 +224,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(9);
     }
 
+    /**
+     *
+     */
     function it_updates_Backstage_pass_items_very_close_the_sell_date()
     {
         $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, 5]);
@@ -188,6 +237,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(4);
     }
 
+    /**
+     *
+     */
     function it_updates_Backstage_pass_items_very_close_to_the_sell_date_at_max_quality()
     {
         $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 50, 5]);
@@ -198,6 +250,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(4);
     }
 
+    /**
+     *
+     */
     function it_updates_Backstage_pass_items_with_one_day_left_to_sell()
     {
         $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, 1]);
@@ -208,6 +263,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(0);
     }
 
+    /**
+     *
+     */
     function it_updates_Backstage_pass_items_with_one_day_left_to_sell_at_max_quality()
     {
         $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 50, 1]);
@@ -218,6 +276,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(0);
     }
 
+    /**
+     *
+     */
     function it_updates_Backstage_pass_items_on_the_sell_date()
     {
         $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, 0]);
@@ -228,6 +289,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-1);
     }
 
+    /**
+     *
+     */
     function it_updates_Backstage_pass_items_after_the_sell_date()
     {
         $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, -1]);
@@ -238,9 +302,12 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-2);
     }
 
+    /**
+     *
+     */
     function it_updates_Conjured_items_before_the_sell_date()
     {
-        $this->beConstructedWith('Conjured Mana Cake', 10, 10);
+        $this->beConstructedThrough('of',['Conjured Mana Cake', 10, 10]);
 
         $this->tick();
 
@@ -248,9 +315,12 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(9);
     }
 
-    function updates_Conjured_items_at_zero_quality()
+    /**
+     *
+     */
+    function it_updates_Conjured_items_at_zero_quality()
     {
-        $this->beConstructedWith('Conjured Mana Cake', 0, 10);
+        $this->beConstructedThrough('of',['Conjured Mana Cake', 0, 10]);
 
         $this->tick();
 
@@ -258,9 +328,12 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(9);
     }
 
-    function updates_Conjured_items_on_the_sell_date()
+    /**
+     *
+     */
+    function it_updates_Conjured_items_on_the_sell_date()
     {
-        $this->beConstructedWith('Conjured Mana Cake', 10, 0);
+        $this->beConstructedThrough('of',['Conjured Mana Cake', 10, 0]);
 
         $this->tick();
 
@@ -268,9 +341,12 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-1);
     }
 
-    function updates_Conjured_items_on_the_sell_date_at_0_quality()
+    /**
+     *
+     */
+    function it_updates_Conjured_items_on_the_sell_date_at_0_quality()
     {
-        $this->beConstructedWith('Conjured Mana Cake', 0, 0);
+        $this->beConstructedThrough('of',['Conjured Mana Cake',  0, 0]);
 
         $this->tick();
 
@@ -278,9 +354,12 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-1);
     }
 
-    function updates_Conjured_items_after_the_sell_date()
+    /**
+     *
+     */
+    function it_updates_Conjured_items_after_the_sell_date()
     {
-        $this->beConstructedWith('Conjured Mana Cake', 10, -10);
+        $this->beConstructedThrough('of',['Conjured Mana Cake', 10, -10]);
 
         $this->tick();
 
@@ -288,9 +367,12 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-11);
     }
 
-    function updates_Conjured_items_after_the_sell_date_at_zero_quality()
+    /**
+     *
+     */
+    function it_updates_Conjured_items_after_the_sell_date_at_zero_quality()
     {
-        $this->beConstructedWith('Conjured Mana Cake', 0, -10);
+        $this->beConstructedThrough('of',['Conjured Mana Cake',  0, -10]);
 
         $this->tick();
 
