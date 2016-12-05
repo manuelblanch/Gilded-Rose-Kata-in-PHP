e4984aea34f5225d58a846546c36d171489b987d
manuelblanch <manuelblanch@iesebre.com>
2016-12-05 13:54:41 +0100

    test back stage

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -148,9 +148,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-1);
     }
 
-    function updates_Backstage_pass_items_long_before_the_sell_date()
+    function it_updates_Backstage_pass_items_long_before_the_sell_date()
     {
-        $this->beConstructedWith('Backstage passes to a TAFKAL80ETC concert', 10, 11);
+        $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, 11]);
 
         $this->tick();
 
@@ -158,9 +158,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(10);
     }
 
-    function updates_Backstage_pass_items_close_to_the_sell_date()
+    function it_updates_Backstage_pass_items_close_to_the_sell_date()
     {
-        $this->beConstructedWith('Backstage passes to a TAFKAL80ETC concert', 10, 10);
+        $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, 10]);
 
         $this->tick();
 
@@ -168,9 +168,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(9);
     }
 
-    function updates_Backstage_pass_items_close_to_the_sell_data_at_max_quality()
+    function it_updates_Backstage_pass_items_close_to_the_sell_data_at_max_quality()
     {
-        $this->beConstructedWith('Backstage passes to a TAFKAL80ETC concert', 50, 10);
+        $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 50, 10]);
 
         $this->tick();
 
@@ -178,9 +178,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(9);
     }
 
-    function updates_Backstage_pass_items_very_close_the_sell_date()
+    function it_updates_Backstage_pass_items_very_close_the_sell_date()
     {
-        $this->beConstructedWith('Backstage passes to a TAFKAL80ETC concert', 10, 5);
+        $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, 5]);
 
         $this->tick();
 
@@ -188,9 +188,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(4);
     }
 
-    function updates_Backstage_pass_items_very_close_to_the_sell_date_at_max_quality()
+    function it_updates_Backstage_pass_items_very_close_to_the_sell_date_at_max_quality()
     {
-        $this->beConstructedWith('Backstage passes to a TAFKAL80ETC concert', 50, 5);
+        $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 50, 5]);
 
         $this->tick();
 
@@ -198,19 +198,19 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(4);
     }
 
-    function updates_Backstage_pass_items_with_one_day_left_to_sell()
+    function it_updates_Backstage_pass_items_with_one_day_left_to_sell()
     {
-        $this->beConstructedWith('Backstage passes to a TAFKAL80ETC concert', 10, 1);
+        $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, 1]);
 
         $this->tick();
 
         $this->quality->shouldBe(13);
-        $this->sellIn->shouldBe(3);
+        $this->sellIn->shouldBe(0);
     }
 
-    function updates_Backstage_pass_items_with_one_day_left_to_sell_at_max_quality()
+    function it_updates_Backstage_pass_items_with_one_day_left_to_sell_at_max_quality()
     {
-        $this->beConstructedWith('Backstage passes to a TAFKAL80ETC concert', 50, 1);
+        $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 50, 1]);
 
         $this->tick();
 
@@ -218,9 +218,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(0);
     }
 
-    function updates_Backstage_pass_items_on_the_sell_date()
+    function it_updates_Backstage_pass_items_on_the_sell_date()
     {
-        $this->beConstructedWith('Backstage passes to a TAFKAL80ETC concert', 10, 0);
+        $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, 0]);
 
         $this->tick();
 
@@ -228,9 +228,9 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-1);
     }
 
-    function updates_Backstage_pass_items_after_the_sell_date()
+    function it_updates_Backstage_pass_items_after_the_sell_date()
     {
-        $this->beConstructedWith('Backstage passes to a TAFKAL80ETC concert', 10, -1);
+        $this->beConstructedThrough('of',['Backstage passes to a TAFKAL80ETC concert', 10, -1]);
 
         $this->tick();
 
@@ -238,7 +238,7 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(-2);
     }
 
-    function updates_Conjured_items_before_the_sell_date()
+    function it_updates_Conjured_items_before_the_sell_date()
     {
         $this->beConstructedWith('Conjured Mana Cake', 10, 10);
 
